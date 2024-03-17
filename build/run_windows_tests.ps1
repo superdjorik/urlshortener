38be5f2f8731010285e051591197af477f08de00
Georgy Solonitsky <georgy.solonitsky@tele2.ru>
2024-03-17 17:19:31 +0300

    small changes build files

--- build/run_windows_tests.ps1
+++ build/run_windows_tests.ps1
@@ -1,9 +1,10 @@
 # cd ..
+go build -o ./cmd/shortener/shortener.exe cmd/shortener/main.go
 $ITERBRANCH = git rev-parse --abbrev-ref HEAD
 if ($ITERBRANCH -match '^iter([0-9]+)$' -eq 'True') {
 $ITERNUM = $ITERBRANCH -replace '^iter([0-9]+)$', '$1'
-for (($i = 1); $i -le $ITERNUM; $i++)
-{.\shortenertestbeta.exe --test.v --test.run=^TestIteration$ITERNUM$ --binary-path=./cmd/shortener/shortener.exe}
+for (($i = 0); $i -le $ITERNUM; $i++)
+{.\shortenertestbeta.exe --test.v --test.run=^TestIteration$ITERNUM$ --binary-path=./cmd/shortener/shortener.exe --source-path=./}
 }
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
